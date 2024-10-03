b8680b2c27e30a87b6e9a87ec76050cd7a26ef27
li-apporto-service <li.service@apporto.com>
2024-10-03 03:11:24 -0400

    Show rdp connection info

--- build.ps1
+++ build.ps1
@@ -1,5 +1,4 @@
-iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+$blockRdp = $true; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
+
 Set-Location "$env:APPVEYOR_BUILD_FOLDER"
 msbuild PLX9x5x.sln /p:Configuration=Release /p:Platform=x64
-
-$blockRdp = $true; iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/appveyor/ci/master/scripts/enable-rdp.ps1'))
